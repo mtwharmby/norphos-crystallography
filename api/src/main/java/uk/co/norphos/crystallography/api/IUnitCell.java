461d404c7b160b74d5d2b13d3319cae6795bba7a
Michael Wharmby <mtwharmby@gmail.com>
2017-08-06 00:39:45 +0200

    Updates POMs to depend on JUnit 4.12

--- api/src/main/java/uk/co/norphos/crystallography/api/IUnitCell.java
+++ api/src/main/java/uk/co/norphos/crystallography/api/IUnitCell.java
@@ -13,244 +13,246 @@ import uk.co.norphos.crystallography.api.maths.Vector;
  *
  */
 public interface IUnitCell extends Comparable<IUnitCell> {
-	
-	/**
-	 * Return the real-space lattice parameters for this IUnitCell.
-	 * @return {@link Lattice}
-	 */
-	Lattice getLattice();
-	
-	/**
-	 * Return the a parameter for the configured lattice.
-	 * @return double length in Angstrom
-	 */
-	default double getA() {
-		return getLattice().getA();
-	}
-	
-	/**
-	 * Return the b parameter for the configured lattice.
-	 * @return double length in Angstrom
-	 */
-	default double getB() {
-		return getLattice().getB();
-	}
-	
-	/**
-	 * Return the c parameter for the configured lattice.
-	 * @return double length in Angstrom
-	 */
-	default double getC() {
-		return getLattice().getC();
-	}
-	
-	/**
-	 * Return the alpha parameter for the configured lattice.
-	 * @return double angle in degrees
-	 */
-	default double getAlpha() {
-		return getLattice().getAl();
-	}
-	
-	/**
-	 * Return the beta parameter for the configured lattice.
-	 * @return double angle in degrees
-	 */
-	default double getBeta() {
-		return getLattice().getBe();
-	}
-	
-	/**
-	 * Return the gamma parameter for the configured lattice.
-	 * @return double angle in degrees
-	 */
-	default double getGamma() {
-		return getLattice().getGa();
-	}
-	
-	/**
-	 * Return the volume of the real-space unit cell of the configured lattice.
-	 * @return double unit cell volume in Angstrom<sup>3</sup>
-	 */
-	default double getVolume() {
-		return getLattice().getVolume();
-	}
-	
-	/**
-	 * Return the lattice crystal system.
-	 * @return {@link CrystalSystem}
-	 */
-	default CrystalSystem getCrystalSystem() {
-		return getLattice().getCrystalSystem();
-	}
-	
-	/**
-	 * Return the reciprocal-space lattice parameters for this IUnitCell.
-	 * @return {@link Lattice}
-	 */
-	default Lattice getReciprocalLattice() {
-		return getReciprocal().getLattice();
-	}
-	
-	/**
-	 * Return the a* parameter for the determined reciprocal lattice.
-	 * @return double length in Angstrom<sup>-1<sup>
-	 */
-	default double getAStar() {
-		return getReciprocalLattice().getA();
-	}
 
-	/**
-	 * Return the b* parameter for the determined reciprocal lattice.
-	 * @return double length in Angstrom<sup>-1<sup>
-	 */
-	default double getBStar() {
-		return getReciprocalLattice().getB();
-	}
-	
-	/**
-	 * Return the c* parameter for the determined reciprocal lattice.
-	 * @return double length in Angstrom<sup>-1<sup>
-	 */
-	default double getCStar() {
-		return getReciprocalLattice().getC();
-	}
-	
-	/**
-	 * Return the alpha* parameter for the determined reciprocal lattice.
-	 * @return double angle in degrees
-	 */
-	default double getAlphaStar() {
-		return getReciprocalLattice().getAl();
-	}
-	
-	/**
-	 * Return the beta* parameter for the determined reciprocal lattice.
-	 * @return double angle in degrees
-	 */
-	default double getBetaStar() {
-		return getReciprocalLattice().getBe();
-	}
-	
-	/**
-	 * Return the gamma* parameter for the determined reciprocal lattice.
-	 * @return double angle in degrees
-	 */
-	default double getGammaStar() {
-		return getReciprocalLattice().getGa();
-	}
-	
-	/**
-	 * Return the metric tensor (G-matrix) for the real-space unit cell.
-	 * @return Matrix G-matrix
-	 */
-	Matrix getMetricTensor();
-	
-	/**
-	 * Return the metric tensor of the reciprocal-space unit cell.
-	 * @return Matrix reciprocal-space G-matrix
-	 */
-	default Matrix getReciprocalMetricTensor() {
-		return getReciprocal().getMetricTensor();
-	}
-	
-	/**
-	 * Return the reciprocal-space equivalent of this IUnitCell.
-	 * @return IUnitCell
-	 */
-	IUnitCell getReciprocal();
-	
-	/**
-	 * Convert a vector in Cartesian coordinates to its equivalent in the 
-	 * fractional coordinate system of this unit cell.
-	 * 
-	 * @param cartVector Vector in Cartesian coordinates
-	 * @return Vector in fractional coordinates of the current lattice
-	 */
-	default Vector fractionalize(Vector cartVector) {
-		return new Vector(getFractionalizationMatrix().multiply(cartVector.toArray()));
-	}
-	
-	/**
-	 * Convert a vector in fractional coordinates of this unit cell into an 
-	 * equivalent vector in Cartesian coordinates.
-	 * 
-	 * @param fracVector Vector in fractional coordinates
-	 * @return Vector in Cartesian coordinates
-	 */
-	default Vector orthogonalize(Vector fracVector) {
-		return new Vector(getOrthogonalizationMatrix().multiply(fracVector.toArray()));
-	}
-	
-	/**
-	 * Return matrix to convert Cartesian coordinates into fractional 
-	 * coordinates for this unit cell's lattice.
-	 * @return Matrix
-	 */
-	Matrix getFractionalizationMatrix();
-	
-	/**
-	 * Return matrix to convert fractional coordinates of this unit cell's 
-	 * lattice into Cartesian coordinates.
-	 * @return Matrix
-	 */
-	Matrix getOrthogonalizationMatrix();
-	
-	/**
-	 * Calculate the length of a vector specified in fractional coordinates of 
-	 * this unit cell.
-	 * 
-	 * @param fracVec Vector in fractional coordinates
-	 * @return double length of vector
-	 */
-	double calculateLength(Vector fracVec);
-	
-	/**
-	 * Calculate the distance between two sites specified in fractional 
-	 * coordinates of this unit cell.
-	 * 
-	 * @param site1 Vector in fractional coordinates
-	 * @param site2 Vector in fractional coordinates
-	 * @return double distance between sites
-	 */
-	default double calculateDistance(Vector site1, Vector site2) {
-		return calculateLength(site2.subtract(site1));
-	}
-	
-	/**
-	 * Calculate the angle between two vectors specified in fractional 
-	 * coordinates of this unit cell.
-	 * 
-	 * @param fracVec1 Vector in fractional coordinates
-	 * @param fracVec2 Vector in fractional coordinates
-	 * @return double angle between vectors in radians
-	 */
-	double calculateAngle(Vector fracVec1, Vector fracVec2);
-	
-	/**
-	 * Calculate the angle between site 1 and site3 at site2 (i.e. the angle 
-	 * between the vectors site1-site2 and site2-site3, c.f. bond angle).
-	 * 
-	 * @param site1 Vector in fractional coordinates
-	 * @param site2 Vector in fractional coordinates
-	 * @param site3 Vector in fractional coordinates
-	 * @return double angle at site2 in radians
-	 */
-	default double calculateAngle(Vector site1, Vector site2, Vector site3) {
-		return calculateAngle(site2.subtract(site1), site2.subtract(site3));
-	}
-	
-	/**
-	 * Calculate the angle between the planes containing site1, site2 and 
-	 * site3 and site2, site3 and site4.
-	 * 
-	 * @param site1 Vector in fractional coordinates
-	 * @param site2 Vector in fractional coordinates
-	 * @param site3 Vector in fractional coordinates
-	 * @param site4 Vector in fractional coordinates
-	 * @return double angle between planes in radians
-	 */
-	double calculateDihedralAngle(Vector site1, Vector site2, Vector site3, Vector site4);
+    /**
+     * Return the real-space lattice parameters for this IUnitCell.
+     * @return {@link Lattice}
+     */
+    Lattice getLattice();
+
+    /**
+     * Return the a parameter for the configured lattice.
+     * @return double length in Angstrom
+     */
+    default double getA() {
+        return getLattice().getA();
+    }
+
+    /**
+     * Return the b parameter for the configured lattice.
+     * @return double length in Angstrom
+     */
+    default double getB() {
+        return getLattice().getB();
+    }
+
+    /**
+     * Return the c parameter for the configured lattice.
+     * @return double length in Angstrom
+     */
+    default double getC() {
+        return getLattice().getC();
+    }
+
+    /**
+     * Return the alpha parameter for the configured lattice.
+     * @return double angle in degrees
+     */
+    default double getAlpha() {
+        return getLattice().getAl();
+    }
+
+    /**
+     * Return the beta parameter for the configured lattice.
+     * @return double angle in degrees
+     */
+    default double getBeta() {
+        return getLattice().getBe();
+    }
+
+    /**
+     * Return the gamma parameter for the configured lattice.
+     * @return double angle in degrees
+     */
+    default double getGamma() {
+        return getLattice().getGa();
+    }
+
+    /**
+     * Return the volume of the real-space unit cell of the configured lattice.
+     * @return double unit cell volume in Angstrom<sup>3</sup>
+     */
+    default double getVolume() {
+        return getLattice().getVolume();
+    }
+
+    /**
+     * Return the lattice crystal system.
+     * @return {@link CrystalSystem}
+     */
+    default CrystalSystem getCrystalSystem() {
+        return getLattice().getCrystalSystem();
+    }
+
+    /**
+     * Return the reciprocal-space lattice parameters for this IUnitCell.
+     * @return {@link Lattice}
+     */
+    default Lattice getReciprocalLattice() {
+        return getReciprocal().getLattice();
+    }
+
+    /**
+     * Return the a* parameter for the determined reciprocal lattice.
+     * @return double length in Angstrom<sup>-1<sup>
+     */
+    default double getAStar() {
+        return getReciprocalLattice().getA();
+    }
+
+    /**
+     * Return the b* parameter for the determined reciprocal lattice.
+     * @return double length in Angstrom<sup>-1<sup>
+     */
+    default double getBStar() {
+        return getReciprocalLattice().getB();
+    }
+
+    /**
+     * Return the c* parameter for the determined reciprocal lattice.
+     * @return double length in Angstrom<sup>-1<sup>
+     */
+    default double getCStar() {
+        return getReciprocalLattice().getC();
+    }
+
+    /**
+     * Return the alpha* parameter for the determined reciprocal lattice.
+     * @return double angle in degrees
+     */
+    default double getAlphaStar() {
+        return getReciprocalLattice().getAl();
+    }
+
+    /**
+     * Return the beta* parameter for the determined reciprocal lattice.
+     * @return double angle in degrees
+     */
+    default double getBetaStar() {
+        return getReciprocalLattice().getBe();
+    }
+
+    /**
+     * Return the gamma* parameter for the determined reciprocal lattice.
+     * @return double angle in degrees
+     */
+    default double getGammaStar() {
+        return getReciprocalLattice().getGa();
+    }
+
+    /**
+     * Return the metric tensor (G-matrix) for the real-space unit cell.
+     * @return Matrix G-matrix
+     */
+    Matrix getMetricTensor();
+
+    /**
+     * Return the metric tensor of the reciprocal-space unit cell.
+     * @return Matrix reciprocal-space G-matrix
+     */
+    default Matrix getReciprocalMetricTensor() {
+        return getReciprocal().getMetricTensor();
+    }
+
+    /**
+     * Return the reciprocal-space equivalent of this IUnitCell.
+     * @return IUnitCell
+     */
+    IUnitCell getReciprocal();
+
+    /**
+     * Convert a vector in Cartesian coordinates to its equivalent in the
+     * fractional coordinate system of this unit cell.
+     *
+     * @param cartVector Vector in Cartesian coordinates
+     * @return Vector in fractional coordinates of the current lattice
+     */
+    default Vector fractionalize(Vector cartVector) {
+//		return new Vector(getFractionalizationMatrix().multiply(cartVector.toArray()));
+        return null; //FIXME
+    }
+
+    /**
+     * Convert a vector in fractional coordinates of this unit cell into an
+     * equivalent vector in Cartesian coordinates.
+     *
+     * @param fracVector Vector in fractional coordinates
+     * @return Vector in Cartesian coordinates
+     */
+    default Vector orthogonalize(Vector fracVector) {
+//		return new Vector(getOrthogonalizationMatrix().multiply(fracVector.toArray()));
+        return null; //FIXME
+    }
+
+    /**
+     * Return matrix to convert Cartesian coordinates into fractional
+     * coordinates for this unit cell's lattice.
+     * @return Matrix
+     */
+    Matrix getFractionalizationMatrix();
+
+    /**
+     * Return matrix to convert fractional coordinates of this unit cell's
+     * lattice into Cartesian coordinates.
+     * @return Matrix
+     */
+    Matrix getOrthogonalizationMatrix();
+
+    /**
+     * Calculate the length of a vector specified in fractional coordinates of
+     * this unit cell.
+     *
+     * @param fracVec Vector in fractional coordinates
+     * @return double length of vector
+     */
+    double calculateLength(Vector fracVec);
+
+    /**
+     * Calculate the distance between two sites specified in fractional
+     * coordinates of this unit cell.
+     *
+     * @param site1 Vector in fractional coordinates
+     * @param site2 Vector in fractional coordinates
+     * @return double distance between sites
+     */
+    default double calculateDistance(Vector site1, Vector site2) {
+        return calculateLength(site2.subtract(site1));
+    }
+
+    /**
+     * Calculate the angle between two vectors specified in fractional
+     * coordinates of this unit cell.
+     *
+     * @param fracVec1 Vector in fractional coordinates
+     * @param fracVec2 Vector in fractional coordinates
+     * @return double angle between vectors in radians
+     */
+    double calculateAngle(Vector fracVec1, Vector fracVec2);
+
+    /**
+     * Calculate the angle between site 1 and site3 at site2 (i.e. the angle
+     * between the vectors site1-site2 and site2-site3, c.f. bond angle).
+     *
+     * @param site1 Vector in fractional coordinates
+     * @param site2 Vector in fractional coordinates
+     * @param site3 Vector in fractional coordinates
+     * @return double angle at site2 in radians
+     */
+    default double calculateAngle(Vector site1, Vector site2, Vector site3) {
+        return calculateAngle(site2.subtract(site1), site2.subtract(site3));
+    }
+
+    /**
+     * Calculate the angle between the planes containing site1, site2 and
+     * site3 and site2, site3 and site4.
+     *
+     * @param site1 Vector in fractional coordinates
+     * @param site2 Vector in fractional coordinates
+     * @param site3 Vector in fractional coordinates
+     * @param site4 Vector in fractional coordinates
+     * @return double angle between planes in radians
+     */
+    double calculateDihedralAngle(Vector site1, Vector site2, Vector site3, Vector site4);
 //	
 //	/**
 //	 * Maximum {@link MillerPlane} for the given d-spacing limit.
